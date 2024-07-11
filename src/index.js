c7c88163d90ed152f5a18e8e580a9596b7d1bc88
Sean Wangelin <seanwangelin@outlook.com>
2024-07-11 17:42:59 -0500

    basic app set up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components';
+import { BrowserRouter } from 'react-router-dom';
+
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
 import './style/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root'),
+  );
