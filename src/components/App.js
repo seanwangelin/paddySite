388a955e26b6531c875c949fed6a445e25118136
Sean Wangelin <seanwangelin@outlook.com>
2024-07-12 22:14:37 -0500

    crikey I actually got an image rendered from an sql database

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
-import { getAPIHealth } from '../axios-services';
-import '../style/App.css';
+import { getAPIHealth } from "../axios-services";
+import "../style/App.css";
 
 const App = () => {
-  const [APIHealth, setAPIHealth] = useState('');
+  const [APIHealth, setAPIHealth] = useState("");
   const [postsArray, setPostsArray] = useState([]);
+
+  const DB = "http://localhost:4000"
 
   useEffect(() => {
     // follow this pattern inside your useEffect calls:
@@ -16,7 +18,7 @@ const App = () => {
     // invoke the adapter, await the response, and set the data
     const getAPIStatus = async () => {
       const { healthy } = await getAPIHealth();
-      setAPIHealth(healthy ? 'api is up! :D' : 'api is down :/');
+      setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
     };
 
     // second, after you've defined your getter above
@@ -40,17 +42,35 @@ const App = () => {
       });
       setPostsArray(posts);
 
-      return result;
+      console.log(result)
+      // return result;
     } catch (err) {
       throw err;
     }
   };
 
+  useEffect(() => {
+    getPosts();
+  }, [])
+
   return (
-    <div className="app-container">
-      <h1>Hello, World!</h1>
-      <p>API Status: {APIHealth}</p>
-    </div>
+    <>
+      <div className="app-container">
+        <h1>Hello, World!</h1>
+        <p>API Status: {APIHealth}</p>
+      </div>
+      <div id='postContainer'>
+        {postsArray.map((post) => {
+          return (
+            <>
+              <div key={post.id}>
+                <img src={post.image_url}/>
+              </div>
+            </>
+          )
+        })}
+      </div>
+    </>
   );
 };
 
