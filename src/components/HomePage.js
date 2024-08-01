67c9d52a2e29803a3b9c5c2d31740c11e941484c
Sean Wangelin <seanwangelin@outlook.com>
2024-07-31 22:04:43 -0500

    removing unneccessary imports

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
-import { React, useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { React } from "react";
+import { useNavigate } from "react-router-dom";
 import "../style/HomePage.css";
 
 export default function HomePage({ DB, postsArray, admin }) {
